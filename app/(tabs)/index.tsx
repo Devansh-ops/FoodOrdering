a0a10ec06989e243529a132d49a59e929f7b45f9
Devansh Sehgal <devanshsehgal02@gmail.com>
2024-03-10 13:34:18 +0530

    remove unused components and imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,4 @@
-import { StyleSheet, Image } from 'react-native';
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import Colors from '@/constants/Colors';
+import { View } from '@/components/Themed';
 import products from '@/assets/data/products';
 
 import ProductListItem from '@/components/ProductListItem';
@@ -14,24 +11,3 @@ export default function MenuScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-});
