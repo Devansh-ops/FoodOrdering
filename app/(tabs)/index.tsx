b3e7d49e42d54c52229dafebb1d7b67ac6d84f48
Devansh Sehgal <devanshsehgal02@gmail.com>
2024-03-10 12:54:35 +0530

    add images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import Colors from '@/constants/Colors';
+import products from '@/assets/data/products';
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={product.image} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
@@ -19,13 +22,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
   },
 });
---
+++ assets/data/products.ts
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: 'Ultimate Pepperoni',
+    image:
+      require('@/assets/images/bundle/peperoni.png'),
+    price: 12.99,
+  },
+  {
+    id: 2,
+    name: 'ExtravaganZZa',
+    image:
+      require('@/assets/images/bundle/extravaganzza.png'),
+    price: 14.99,
+  },
+  {
+    id: 3,
+    name: 'MeatZZa',
+    image:
+      require('@/assets/images/bundle/peperoni.png'),
+    price: 13.47,
+  },
+  {
+    id: 4,
+    name: 'Margarita',
+    image:
+      require('@/assets/images/bundle/margarita.png'),
+    price: 9.9,
+  },
+  {
+    id: 5,
+    name: 'Pacific Veggie',
+    image:
+      require('@/assets/images/bundle/veggie.png'),
+    price: 12.99,
+  },
+  {
+    id: 6,
+    name: 'Hawaiian',
+    image:
+      require('@/assets/images/bundle/hawaiin.png'),
+    price: 10.49,
+  },
+  {
+    id: 7,
+    name: 'Deluxe',
+    image:
+      require('@/assets/images/bundle/deluxe.png'),
+    price: 16.99,
+  },
+  {
+    id: 8,
+    name: 'BBQ Chicken',
+    image:
+      require('@/assets/images/bundle/veggie.png'),
+    price: 12.89,
+  },
+  {
+    id: 9,
+    name: 'Chicken Bacon Ranch',
+    image:
+      require('@/assets/images/bundle/extravaganzza.png'),
+    price: 13.99,
+  },
+  {
+    id: 10,
+    name: '6 Cheese',
+    image:
+      require('@/assets/images/bundle/6cheese.png'),
+    price: 13.29,
+  },
+];
+
+export default products;
